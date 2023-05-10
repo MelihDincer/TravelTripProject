0cbf733156b407463cb84d384a9781e0e6279a11
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-10 17:30:34 +0300

    Yorum ve kod satırları düzenlendi.

--- TravelTripProje/Controllers/AdminController.cs
+++ TravelTripProje/Controllers/AdminController.cs
@@ -9,21 +9,17 @@ namespace TravelTripProje.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
-
         Context c = new Context();
         public ActionResult Index()
         {
             var degerler = c.Blogs.ToList();
             return View(degerler);
         }
-
         [HttpGet] //sayfa yüklendiğinde
         public ActionResult YeniBlog()
         {
             return View(); //sayfanın boş halini döndür.
         }
-
         [HttpPost] //sayfada işlem yapıldığında
         public ActionResult YeniBlog(Blog p)
         {
@@ -31,24 +27,20 @@ namespace TravelTripProje.Controllers
             c.SaveChanges();
             return RedirectToAction("Index"); //Index aksiyonuna yönlendir.
         }
-
         public ActionResult BlogSil(int id)
         {
             var b = c.Blogs.Find(id); //b değerinde değişken tanımla, bu da c'nin içindeki bloglarda id yi bulsun.
             c.Blogs.Remove(b); // c'nin içindeki bloglar için tanımladığımız b değeri silinsin.
-            c.SaveChanges(); // kaydet
-            return RedirectToAction("Index"); //index 'e yönlendir
+            c.SaveChanges(); // değişiklikleri kaydet
+            return RedirectToAction("Index"); //index sayfasına yönlendir
         }
-
-
-
-        //GÜNCELLEME İÇİN KODLAR
+        //Güncellenecek olan blogun bilgilerini otomatik olarak doldur.
         public ActionResult BlogGetir(int id)
         {
             var bl = c.Blogs.Find(id);
             return View("BlogGetir", bl); // blog getir sayfasını döndür ve bl'den gelen değerleri de getir.
         }
-
+        //Güncelleme İşlemleri
         public ActionResult BlogGuncelle(Blog b)
         {
             var blg = c.Blogs.Find(b.ID);
@@ -60,5 +52,4 @@ namespace TravelTripProje.Controllers
             return RedirectToAction("Index");
         }
     }
-
 }
