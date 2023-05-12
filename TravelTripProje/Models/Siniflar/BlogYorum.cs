a7c57e03bb7adec90d351625a160b45fdd8b2a11
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-13 00:49:06 +0300

    Son yorumlar için gerekli olan property tanımlandı.

--- TravelTripProje/Models/Siniflar/BlogYorum.cs
+++ TravelTripProje/Models/Siniflar/BlogYorum.cs
@@ -9,6 +9,7 @@ namespace TravelTripProje.Models.Siniflar
     {
         public IEnumerable<Blog> Deger1 { get; set; } //bir view içerisine birden fazla tablodan veri çekebilmek için "IEnumerable" yapısını kullandık.
         public IEnumerable<Yorumlar> Deger2 { get; set; }
-        public IEnumerable<Blog> Deger3 { get; set; } //son bloglar kısmında son blogları listelemek için deger3 adında bir property oluşturduk
+        public IEnumerable<Blog> Deger3 { get; set; } //son bloglar kısmında son blogları listelemek için deger3 adında bir property oluşturduk.
+        public IEnumerable <Yorumlar> Deger4 { get; set; }//son yorumlar kısmında son blogları listelemek için deger3 adında bir property oluşturduk.
     }
 }
